da1a586f4aa35422b6c6c1c5330f7ff1f81144e0
Richard Yan <richard.yan@intel.com>
2022-08-18 09:08:43 -0700

    final changes

--- untitled1.py
+++ untitled1.py
@@ -5,11 +5,11 @@ Created on Wed Aug 17 16:05:56 2022
 @author: yanricha
 """
 
-<<<<<<< HEAD
-=======
+
 print("Hello World error")
->>>>>>> 1b587899d1b26385b7c1d758f881f2215fd5974e
+
 print("change 1")
+
 print("change 2")
 print("branch try again")
 print("first pull attempt")
