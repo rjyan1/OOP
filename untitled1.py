619bd70179b3206df1d97cfa85632c3515fc2b78
Richard Yan <richard.yan@intel.com>
2022-08-24 09:35:31 -0700

    OOP 29M 29S

--- untitled1.py
+++ untitled1.py
@@ -1,19 +1,37 @@
 class Item:
+    def __init__(self, name:str, price:float, quantity):
+        # Run Validations to the received arguments
+        assert price >= 0
+        assert quantity >= 0
+        
+        # Assign to self object
+        print(f"An instance created: {name}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.calculate = quantity * price
     def calculate_total_price(self, x, y):
         return x * y
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price , item1.quantity))
+item1 = Item("Phone", -100, 5)
 
-item2 = Item()
-item2.name = "DVI Tool"
-item2.price = 4000
-item2.quantity = 4
-print('The total price is $',item2.calculate_total_price(item2.price,item2.quantity))
+
+item2 = Item("DVI Tool", 4000, 4)
+
+print(item1.name)
+print(item2.name)
+print(item1.price)
+print(item2.price)
+print(item1.quantity)
+print(item2.quantity)
+print(item1.calculate)
+
+
+
+
+
+
 
 
 
