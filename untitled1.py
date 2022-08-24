a78bc4374c301f4122718e453b08e265f77a2d76
Richard Yan <richard.yan@intel.com>
2022-08-24 09:00:53 -0700

    updated code

--- untitled1.py
+++ untitled1.py
@@ -1,13 +1,20 @@
 class Item:
-    pass
+    def calculate_total_price(self, x, y):
+        return x * y
 
 
 item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate_total_price(item1.price , item1.quantity))
 
-random_str = "aaa"
-print(random_str.upper())
+item2 = Item()
+item2.name = "DVI Tool"
+item2.price = 4000
+item2.quantity = 4
+print('The total price is $',item2.calculate_total_price(item2.price,item2.quantity))
+
+
 
 
