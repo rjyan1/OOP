1f992f854f2d9de163a96ae2f44a5d6a51def2ee
Richard Yan <richard.yan@intel.com>
2022-08-24 08:38:11 -0700

    starting fresh

--- untitled1.py
+++ untitled1.py
@@ -1,17 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 17 16:05:56 2022
 
-@author: yanricha
-"""
-
-
-print("Hello World error")
-
-print("change 1")
-
-print("change 2")
-print("branch try again")
-print("first pull attempt")
-print("another push attempt")
-print("another pull request")
